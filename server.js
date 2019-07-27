3cfb4cd668e2d12cda6f5683ca0f9367efcd4fff
YasirAlharbi <46800088+YasirAlharbi@users.noreply.github.com>
2019-07-27 15:50:31 +0300

    Update server.js

--- server.js
+++ server.js
@@ -9,18 +9,22 @@ const app = express ();
 app.use(bodyParser.json());
 
 const db = require ('./config/keys').mongoURI;
-const { MongoClient } = require("mongodb");
+//const  MongoClient  = require('mongodb').MongoClient;
 
-//const uri = 'mongodb+srv://localhost:27017';  // mongodb://localhost - will fail
+const MongoClient = require('mongodb').MongoClient;
 
+const items = require ('./routes/api/items');
+//const uri = 'mongodb+srv://yasir:100100Yy@cluster0-gdzcq.mongodb.net/test';  // mongodb://localhost - will fail
 
+const port = process.env.PORT || 27017;
 mongoose
   .connect(db) 
   .then(() => console.log('MongoDB connected ....'))
   .catch(err => console.log(err));
   
-  mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true });
-  const port = process.env.PORT ||27017;
-
-  app.listen(port, () => console.log('Server started  on port ${port}'));
+  //mongoose.connect('mongodb://localhost:27017/mDB/names', { useNewUrlParser: true });
+  //const port = process.env.PORT || 27017;
+app.use('/api/items', items);
+app.listen(port, () => console.log('Server started on port ' + port));
+  
 
